67a27a352712d6ce753a78211f269e1d9141c222
luci0310 <lucianamartinez898@gmail.com>
2023-06-29 11:12:07 -0300

    creamos el componente items y una clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ItemsComponent } from './components/items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items:Item[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.items= [
+      {
+        id: 0,
+        title: 'manzana',
+        price: 10.5,
+        quantity: 4,
+        completed:false
+      },
+      {
+        id: 1,
+        title: 'pan',
+        price: 3.5,
+        quantity: 8,
+        completed:true
+      }
+    ]
+  }
+}
