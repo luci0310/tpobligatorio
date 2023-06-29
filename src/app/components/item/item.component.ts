35f8481eba015685fed2bc72e3ba9e9279798da9
luci0310 <lucianamartinez898@gmail.com>
2023-06-29 11:40:32 -0300

    creamos el componente item

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Item } from 'src/app/models/item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+@Input() item: Item= new Item(); 
+constructor(){}
+
+ngOnInit(): void{
+
+}
+}
